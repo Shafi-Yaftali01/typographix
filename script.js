4b411a1f040916c759c7be98e82e235d65e8131c
Shafi <shafiyaftali001@gmail.com>
2024-08-25 00:18:59 -0400

    add the dakr mode functionality to the website

---
+++ script.js
@@ -0,0 +1,50 @@
+const themeSwitcher = document.getElementById('theme-switcher');
+
+// Dark Mode Style 
+
+function darkMode() {
+  themeSwitcher.children[0].textContent = 'Dark Mode';
+  themeSwitcher.children[1].classList.replace('fa-sun', 'fa-moon');
+}
+
+function lightMode() {
+  themeSwitcher.children[0].textContent = 'Light Mode';
+  themeSwitcher.children[1].classList.replace('fa-moon', 'fa-sun');
+}
+
+// Switch Theme 
+
+function switchTheme() {
+  const currentTheme = document.documentElement.getAttribute('data-theme');
+  if(!currentTheme || currentTheme === 'light') {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+    darkMode();
+
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+    lightMode();
+  }
+
+
+}
+
+  // Even Listener 
+
+  themeSwitcher.addEventListener('click', switchTheme);
+
+
+  // Check Local Storage for Theme 
+
+  const currentThemeFromLocalStorage = localStorage.getItem('theme');
+
+  if(currentThemeFromLocalStorage) {
+    document.documentElement.setAttribute('data-theme', currentThemeFromLocalStorage);
+    if(currentThemeFromLocalStorage === 'dark') {
+      darkMode();
+    }
+    else {
+      lightMode();
+    }
+  }
