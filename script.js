2dec42153630a6bba225eb5ae5307f21fe775990
Shafi <shafiyaftali001@gmail.com>
2024-08-26 13:33:28 -0400

    Added the responsive the desktop and smartphones

--- script.js
+++ script.js
@@ -48,3 +48,19 @@ function switchTheme() {
       lightMode();
     }
   }
+
+
+  // Navigation  
+
+  const nav = document.getElementById('nav');
+const menuIcon = document.querySelector('.menu-icon');
+
+function toggleMenu() {
+  nav.classList.toggle('active');
+  menuIcon.classList.toggle('active');
+}
+
+function hideMenu() {
+  nav.classList.remove('active');
+  menuIcon.classList.remove('active');
+}
